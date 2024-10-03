53932ee05e3c7c50fff5a33e8511113158697028
definaru <defina.inc@yandex.ru>
2024-10-03 18:52:34 +0300

    added mobile version

---
+++ _next/static/quiz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/69f81e06845cd7ae.css","static/chunks/270-00d4368242c62c2a.js","static/chunks/pages/index-a29e3c7c06105c2e.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
