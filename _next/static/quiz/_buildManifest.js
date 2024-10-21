0e8d0f6e418213b936834ca0ce9854e45813b96e
definaru <defina.inc@yandex.ru>
2024-10-21 22:04:02 +0300

    added update

---
+++ _next/static/quiz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/69f81e06845cd7ae.css","static/chunks/493-d20c3c0c84ede1b3.js","static/chunks/pages/index-38bc25608652fee0.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
