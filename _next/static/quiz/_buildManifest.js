1ab3083b9b398e4d16d51d0fb14ad93700925be6
definaru <defina.inc@yandex.ru>
2024-11-05 00:30:01 +0300

    update index page

---
+++ _next/static/quiz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/9bea1e92561d23d2.css","static/chunks/228-b67043cf53d166aa.js","static/chunks/pages/index-e492074876856833.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
